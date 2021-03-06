7be13ec3c9fe96e3e151039c7595096b971a7a9f
Sergej Herbert <herbert.sergej@gmail.com>
2021-03-06 16:42:14 +0100

    Finish ESP Gyro side logic

--- client/src/functions.ino
+++ client/src/functions.ino
@@ -14,14 +14,24 @@ void measure(void) {
     accel.getEvent(&event);
 
     int side = get_side(event);
+    // retry a couple times
+    if (side == 0) {
+        side = get_side(event);
+    }
+    if (side == 0) {
+        side = get_side(event);
+    }
+    if (side == 0) {
+        side = get_side(event);
+    }
 
+    // if it is still not determined, give up
     if (side == 0) {
         Serial.println("Unknown side");
         Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print(" ");
         Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print(" ");
         Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print(" ");
         Serial.println("m/s^2 ");
-        return;
     }
 
     Serial.print("Side: "); Serial.println(side);
@@ -31,22 +41,49 @@ void measure(void) {
 
 /*
   Determine the "side" based on the event/acceleration. Sides:
-  1: "345 up": the gyroscope is on the side and the side with the number 345 is looking up
-  2: "top": the top of the gyroscope is up
+  1: "PCB bottom up": the bottom of the PCB is up and the text ADXL345 is visible
+  2: "gyroscope up": the top of PCB with the gyroscope is up
+  3: "red-wide": the red wide side of gyroscope is up 
+  4: "grey-wide": the grey wide side of gyroscope is up 
+  5: "red-short": the red short side of gyroscope is up 
+  6: "grey-short": the grey short side of gyroscope is up 
 */
 int get_side(sensors_event_t event) {
     if (
-        event.acceleration.z > 8 &&
+        event.acceleration.z < -7 &&
         abs(event.acceleration.y) + abs(event.acceleration.x) < 7
     ) {
         return 1;
     }
-
     if (
-        event.acceleration.y > 8 &&
-        abs(event.acceleration.z) + abs(event.acceleration.x) < 7
+        event.acceleration.z > 7 &&
+        abs(event.acceleration.y) + abs(event.acceleration.x) < 7
     ) {
         return 2;
+    }
+    if (
+        event.acceleration.x > 7 &&
+        abs(event.acceleration.z) + abs(event.acceleration.y) < 7
+    ) {
+        return 3;
+    }
+    if (
+        event.acceleration.x < -7 &&
+        abs(event.acceleration.z) + abs(event.acceleration.y) < 7
+    ) {
+        return 4;
+    }
+    if (
+        event.acceleration.y < -7 &&
+        abs(event.acceleration.z) + abs(event.acceleration.x) < 7
+    ) {
+        return 5;
+    }
+    if (
+        event.acceleration.y > 7 &&
+        abs(event.acceleration.z) + abs(event.acceleration.x) < 7
+    ) {
+        return 6;
     }
 
     return 0;
@@ -71,7 +108,7 @@ void send_side_to_server(int side) {
     String endpoint = "/gyro/logs?side=" + String(side);
     HTTPClient http;
 
-    Serial.println(baseURL + endpoint);
+    Serial.print("POST to: "); Serial.println(baseURL + endpoint);
     http.begin(baseURL + endpoint);
     http.addHeader("Authorization", "Basic " + auth);
 
--- client/src/gyro_client.ino
+++ client/src/gyro_client.ino
@@ -12,37 +12,36 @@ Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 void setup(void) {
     Serial.begin(9600);
 
-   if (!connect_to_wifi()) {
+    if (!connect_to_wifi()) {
         Serial.println("Error: Connecting to Wifi ...");
         Serial.println("Going into deep sleep mode");
         delay(500);
         ESP.deepSleep(0);
-   } else {
+    } else {
         Serial.println("Connected to Wifi");
-   }
+    }
 
-   /* Initialise the sensor */
-   if (!accel.begin()) {
+    /* Initialise the sensor */
+    if (!accel.begin()) {
         Serial.println("Error: no ADXL345 detected ... Check wiring ...");
         Serial.println("Going into deep sleep mode");
         delay(500);
         ESP.deepSleep(SLEEP_DURATION);
-   }
-   accel.setRange(ADXL345_RANGE_2_G);
+    }
+    accel.setRange(ADXL345_RANGE_2_G);
 
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH); // logic is inverted: HIGH -> off
 
    Serial.println("Setup finished");
    delay(500);
 }
  
 void loop(void) {
-     measure();
-     digitalWrite(LED_BUILTIN, LOW); // off
-     delay(5000);
-     digitalWrite(LED_BUILTIN, HIGH);  // on
+    digitalWrite(LED_BUILTIN, LOW); // logic is inverted: LOW -> on
+    measure();
+    digitalWrite(LED_BUILTIN, HIGH);  // logic is inverted: HIGH -> off
 
     Serial.println("Finished. Going into sleep for (seconds): " + String(SLEEP_DURATION / 1000000));
 
