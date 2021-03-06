557fc1eb1d883ba9f1273b5c44d56b54b6c417d7
Sergej Herbert <herbert.sergej@gmail.com>
2021-03-06 19:38:10 +0100

    Add more default sides

--- migrations/01_gyro_sides_table.sql
+++ migrations/01_gyro_sides_table.sql
@@ -4,4 +4,12 @@ CREATE TABLE gyro_sides(
     , name TEXT
 );
 
-INSERT INTO gyro_sides(timestamp, side, name) VALUES (0, 0, "unknown");
+INSERT INTO gyro_sides(timestamp, side, name) VALUES
+(0, 0, 'unknown')
+, (11, 1, 'PCB bottom up')
+, (22, 2, 'gyroscope up')
+, (33, 3, 'red-wide')
+, (44, 4, 'grey-wide')
+, (55, 5, 'red-short')
+, (66, 6, 'grey-short')
+;
